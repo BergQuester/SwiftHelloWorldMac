caad5877856d012efeb0b12c5283bf511224022b
Daniel Bergquist <daniel.bergquist@gmail.com>
2014-06-03 22:07:37 -0500

    Initial Commit

---
+++ SwiftHelloWorldMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftHelloWorldMac
+//
+//  Created by Daniel Bergquist on 6/3/14.
+//  Copyright (c) 2014 Daniel Bergquist. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftHelloWorldMac/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftHelloWorldMac
+//
+//  Created by Daniel Bergquist on 6/3/14.
+//  Copyright (c) 2014 Daniel Bergquist. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
